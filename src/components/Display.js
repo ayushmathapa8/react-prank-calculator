70228fd28fb0a816be98718732f4cd76c7955a06
Ayushma Thapa <ayushmathapa100@gmail.com>
2022-06-15 11:11:07 +1000

    issue fixe, disply class name not work

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const Display = ({ str, isPrank }) => {
-  const clsNm = isPrank ? "display prank" : Display;
+  const clsNm = isPrank ? "display prank" : "display";
   return <div className={clsNm}> {str || "0.0"}</div>;
 };
 
